00ddb203519c07e0c2dc944261073f22fafca4d2
mahedi898 <mahedihasan2965@gmail.com>
2024-12-30 01:04:06 +0600

    secend commit

--- test.js
+++
@@ -1,9 +0,0 @@
-function getTimeString(time) {
-    const hour = parseInt(time / 3600);
-    let remainingSecond = time % 3600;
-    const minute = parseInt(remainingSecond/60);
-    remainingSecond = remainingSecond % 60;
-    return`${hour}hour ${minute} minute ${remainingSecond} second ago`
-}
-
-console.log(getTimeString(5000));
